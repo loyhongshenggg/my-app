b74d2240c7c49608592b29436752d74a5e2b08b3
loyhongshenggg <e0725346@u.nus.edu>
2022-10-01 17:48:09 +0800

    navigation

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './app/screens/LoginScreen';
 import HomeScreen from './app/screens/HomeScreen';
 import RegistrationScreen from './app/screens/RegistrationScreen';
-import ForgetPassswordScreen from './app/screens/ForgetPasswordScreen';
+import ForgetPassword from './app/screens/ForgetPasswordScreen';
+import Tabs from './navigation/Tabs';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,8 +15,8 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen options = { {headerShown: false}} name ="login" component={LoginScreen} />
         <Stack.Screen options = { {headerShown: false}} name ="register" component={RegistrationScreen} /> 
-        <Stack.Screen options = { {headerShown: false}} name ="home" component={HomeScreen} /> 
-        <Stack.Screen options = { {headerShown: false}} name ="forgetPassword" component={ForgetPassswordScreen} /> 
+        <Stack.Screen options = { {headerShown: false}} name ="forgetpassword" component={ForgetPassword} /> 
+        <Stack.Screen options = { {headerShown: false}} name ="tabs" component={Tabs} /> 
       </Stack.Navigator>
     </NavigationContainer>
 
---
+++ app/screens/CoursesScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function CoursesScreen() {
+  return (
+    <Text>Courses</Text>
+  )
+}
+
+export default CoursesScreen
